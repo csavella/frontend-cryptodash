b69653b9daa492e6f587a5547d0f9a31bb7a89c6
ericanderson333 <ericsanderson333@gmail.com>
2021-05-09 12:23:19 -0700

    Added in comments

--- src/Tables/CryptoTable.js
+++ src/Tables/CryptoTable.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect} from 'react'; 
 import axios from 'axios';
 import CryptoRow from './CryptoRow';
 import {Container, Table} from 'react-bootstrap';
@@ -7,21 +7,25 @@ import './CryptoTable.css';
 
 
 function CryptoTable(){
-    let [coins, setCoins] = useState([]);
+    /* coins: all coin data from api 
+       setCoins: setter for coins data */
+    let [coins, setCoins] = useState([]); 
     const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
+    /* Use effect in replace of componentDidMount() w/ Hooks */
     useEffect(() => {
         axios.get(url)
         .then(res =>{
             setCoins(res.data);
-            //console.log(res.data);
         })
         .catch(error => console.log(error))
     }, []);
 
-    /* Map Function */
+    /* Render data by mapping & creating each 'CryptoRow' component
+       renderCryptoData() called in returning statement on line:73 */
     let renderCryptoData = () => {
         return coins.map((coin, index) => {
+            // Note: All assigned values match params in 'CryptoRow'
             return(
                 <CryptoRow 
                 rank={coin.market_cap_rank}

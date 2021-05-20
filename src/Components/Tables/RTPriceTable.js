150c95a79a55e068d9607b556dc3e84f70e5b4a0
Chasity Savella <csavella@pdx.edu>
2021-05-20 15:44:54 -0700

    Edit Table button now shows Delete column on table when clicked

--- src/Components/Tables/RTPriceTable.js
+++ src/Components/Tables/RTPriceTable.js
@@ -1,8 +1,84 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import RTPriceRows from "./RTPriceRows";
-import { Container, Table } from "react-bootstrap";
+import { Container, Form } from "react-bootstrap";
+import BootstrapTable from "react-bootstrap-table-next";
+import ToolkitProvider from "react-bootstrap-table2-toolkit";
 import "./RTPriceTable.css";
+
+const columns = [
+  {
+    dataField: "rank",
+    text: "Rank",
+  },
+  {
+    dataField: "img",
+    text: "Coin",
+    formatter: imgFormatter,
+  },
+  {
+    dataField: "price",
+    text: "Price",
+  },
+  {
+    dataField: "percentageChange",
+    text: "24hr Change",
+  },
+  {
+    dataField: "volume",
+    text: "24hr Volume",
+  },
+  {
+    dataField: "marketCap",
+    text: "Market Cap",
+  },
+  {
+    dataField: "supply",
+    text: "Max Supply",
+  },
+  {
+    dataField: "toDelete",
+    text: "Delete",
+    hidden: true,
+  },
+];
+
+function imgFormatter(cell, row) {
+  return (
+    <img
+      src={cell}
+      alt={cell.symbol + "logo"}
+      className="coin-item"
+      style={({ height: 2 + "em" }, { width: 2 + "em" })}
+    />
+  );
+}
+
+const CustomToggleList = ({ columns, onColumnToggle, toggles }) => (
+  <div className="text-center">
+    {columns
+      .map((column) => ({
+        ...column,
+        toggle: toggles[column.dataField],
+      }))
+      .map((column, index) =>
+        index === 0 ? (
+          <Form.Check
+            type="button"
+            key={column.dataField}
+            inline
+            label="Edit Table"
+            id={column.dataField}
+            checked={column.toggle}
+            aria-checked={column.toggle ? "true" : "false"}
+            onClick={() =>
+              //onColumnToggle(column.dataField),
+              onColumnToggle(columns[columns.length - 1].dataField)
+            }
+          />
+        ) : null
+      )}
+  </div>
+);
 
 function RTPriceTable() {
   //only get top 10 coins
@@ -31,61 +107,60 @@ function RTPriceTable() {
 
   /* Render data by mapping & creating each 'CryptoRow' component */
   let renderCryptoData = () => {
-    return coins.map((coin, index) => {
-      return (
-        <RTPriceRows
-          rank={coin.market_cap_rank}
-          img={coin.image}
-          symbol={coin.symbol}
-          price={coin.current_price}
-          percentageChange={coin.price_change_percentage_24h}
-          volume={coin.total_volume}
-          marketCap={coin.market_cap}
-          supply={coin.circulating_supply}
-          toDelete="button"
-        />
-      );
-    });
+    let coinData = [];
+    for (let i = 0; i < coins.length; i++) {
+      let tempObject = {
+        rank: coins[i].market_cap_rank,
+        img: coins[i].image,
+        symbol: coins[i].symbol,
+        price: coins[i].current_price,
+        percentageChange: coins[i].price_change_percentage_24h,
+        volume: coins[i].total_volume,
+        marketCap: coins[i].market_cap,
+        supply: coins[i].circulating_supply,
+        toDelete: "button",
+      };
+      coinData.push(tempObject);
+    }
+    return coinData;
   };
 
   return (
     <Container>
       <div className="row justify-content-between rt-price-table-header">
         <h2 className="col-8 temp-title">Real Time Performance</h2>
-        <button type="button" className="col-3 edit-table-link btn btn-link">Edit Table</button>
       </div>
       <div className="row">
-      <Table striped bordered hover size="sm" responsive className="col-12">
-        <thead className="crypto-home-header">
-          <tr>
-            <th>
-              <p>Rank</p>
-            </th>
-            <th>
-              <p>Coin</p>
-            </th>
-            <th>
-              <p>Price</p>
-            </th>
-            <th>
-              <p>24hr %</p>
-            </th>
-            <th>
-              <p>24hr Volume</p>
-            </th>
-            <th>
-              <p>Market Cap</p>
-            </th>
-            <th>
-              <p>Max Supply</p>
-            </th>
-            <th className="delete">
-              <p>Delete</p>
-            </th>
-          </tr>
-        </thead>
-        <tbody>{renderCryptoData()}</tbody>
-      </Table>
+        <ToolkitProvider
+          keyField="id"
+          data={renderCryptoData()}
+          columns={columns}
+          columnToggle
+        >
+          {(props) => (
+            <div>
+              <div
+                className="card card-body"
+                style={{
+                  padding: 5 + "px",
+                  display: "block",
+                }}
+              >
+                <CustomToggleList {...props.columnToggleProps} />
+              </div>
+              <hr />
+              <BootstrapTable
+                striped
+                bordered
+                hover
+                size="sm"
+                responsive
+                className="col-12"
+                {...props.baseProps}
+              />
+            </div>
+          )}
+        </ToolkitProvider>
       </div>
     </Container>
   );

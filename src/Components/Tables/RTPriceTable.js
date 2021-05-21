2feb7534b0739c543027b55e7ea6e137e0b8dde7
Chasity Savella <csavella@pdx.edu>
2021-05-21 16:05:12 -0700

    Pagination and search bar

--- src/Components/Tables/RTPriceTable.js
+++ src/Components/Tables/RTPriceTable.js
@@ -2,14 +2,18 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Container } from "react-bootstrap";
 import BootstrapTable from "react-bootstrap-table-next";
-import ToolkitProvider from "react-bootstrap-table2-toolkit";
+import ToolkitProvider, { Search } from "react-bootstrap-table2-toolkit";
+import "react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css";
+import paginationFactory from "react-bootstrap-table2-paginator";
 import ToggleList from "./ToggleList";
 import "./RTPriceTable.css";
+import { faBorderNone } from "@fortawesome/free-solid-svg-icons";
 
-function RTPriceTable({columns}) {
+function RTPriceTable({ columns }) {
+  const { SearchBar } = Search;
   const [coins, setCoins] = useState([]);
   const url =
-    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
+    "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false";
 
   //only get top 10 coins
   const callAPI = async () => {
@@ -44,7 +48,8 @@ function RTPriceTable({columns}) {
         percentageChange: coins[i].price_change_percentage_24h,
         volume: coins[i].total_volume,
         marketCap: coins[i].market_cap,
-        supply: coins[i].circulating_supply + " " + coins[i].symbol.toUpperCase(),
+        supply:
+          coins[i].circulating_supply + " " + coins[i].symbol.toUpperCase(),
         //toDelete: "button",
       };
       coinData.push(tempObject);
@@ -63,20 +68,38 @@ function RTPriceTable({columns}) {
           data={renderCryptoData()}
           columns={columns}
           columnToggle
+          search
         >
           {(props) => (
             <div>
-              <div
-                className="card card-body"
-                style={{
-                  padding: 5 + "px",
-                  display: "block",
-                }}
-              >
-                <ToggleList columns={columns} {...props.columnToggleProps} />
+              <div className="row justify-content-end">
+                <div className="card card-body col-8" style={{
+                    padding: 5 + "px",
+                    border: "none",
+                  }}></div>
+                <div
+                  className="card card-body col-1 mr-auto"
+                  style={{
+                    padding: 5 + "px",
+                    border: "none", 
+                    textAlign: "left", 
+                  }}
+                >
+                  <ToggleList columns={columns} {...props.columnToggleProps} />
+                </div>
+                <div
+                  className="card card-body col-2"
+                  style={{
+                    padding: 5 + "px",
+                    border: "none",
+                  }}
+                >
+                  <SearchBar {...props.searchProps} />
+                </div>
               </div>
               <hr />
               <BootstrapTable
+                pagination={paginationFactory()}
                 striped
                 bordered
                 hover

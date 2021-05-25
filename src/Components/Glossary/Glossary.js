51c932fc997f0b6f3ffbcc43dc45c7f7468599bf
ericanderson333 <ericsanderson333@gmail.com>
2021-05-25 11:53:27 -0700

    not much styles. All functionalities work

--- src/Components/Glossary/Glossary.js
+++ src/Components/Glossary/Glossary.js
@@ -1,7 +1,42 @@
 import React from 'react';
 import './Glossary.css';
+import data from './Glossary.json';
+
+
 
 const Glossary = () =>{
-    return (<div>Glossary</div>)
+
+    const handleScroll = (e) =>{
+        document.getElementById("item-" + e.target.value).scrollIntoView();
+    }
+
+    return(
+        <div className="glossary-container">
+            <div className="glossary-header-container">
+                {Object.keys(data).map(key => {
+                    return(
+                        <span><input type="button" value={key} onClick={e => handleScroll(e)}/></span>
+                    )
+                })}
+            </div>
+                {Object.keys(data).map(key => {
+                    return(
+                        <div className="glossary-body-container">
+                            <div className={"item"} id={"item-" + key}><h1>{key}</h1></div>
+                            <div className={"item-definitions"}>
+                                {Object.keys(data[key]).map(item => {
+                                    return(
+                                        <div className={"definition-" + data[key][item].term + "-container"}>
+                                            <div className={"term"}><span>{data[key][item].term + ":"}</span></div>
+                                            <div className={"definition"}><span>{data[key][item].description}</span></div>
+                                        </div>
+                                    )
+                                })}
+                            </div>
+                        </div>
+                    )
+                })}
+        </div>
+    )
 }
 export default Glossary;

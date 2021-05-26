787b4dcc1e2fc4ccff24fc7a2c47ef54f35263ce
Chasity Savella <csavella@pdx.edu>
2021-05-25 19:38:21 -0700

    Revert "Update Home.js. Don't know why it didn't pull my changes. Adding them back in"
    This reverts commit e964484323246bb3a2cd4be5dfcfedcba9474d25.

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -2,13 +2,10 @@ import RTPriceFormattedTable from "../Tables/RTPriceFormattedTable";
 import TradingChart from "../TradingChart";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const Home = ({ favoriteCoins, setFavoriteCoins }) => {
+const Home = () => {
   return (
       <div>
-          <RTPriceFormattedTable
-            favoriteCoins={favoriteCoins}
-            setFavoriteCoins={setFavoriteCoins}
-          />
+          <RTPriceFormattedTable />
           <TradingChart />
       </div>
   

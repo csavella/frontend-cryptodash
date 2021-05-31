805cc28445e031dda32f1a1534c89380fac8ba0c
csavella <csavella@pdx.edu>
2021-05-31 14:54:06 -0700

    Fixing margin

--- src/Components/Pages/About.js
+++ src/Components/Pages/About.js
@@ -3,7 +3,7 @@ import "./About.css";
 
 const About = () => {
   return (
-    <Container className="justify-content-center resources-page">
+    <Container className="justify-content-center about-page" style={{margin:"auto"}}>
       <h2 style={{ textAlign: "center", margin: 2 + "em" }}>About</h2>
       <img
         className="img-fluid rounded mx-auto d-block about-img"
